3d5ed66bcfe5cf6b4d6ada5d31a11fca6596d7ae
Diego Alonzo Ochochoque <diegoalonzoochoque@gmail.com>
2026-01-27 18:30:54 -0500

    /InfrastructureRegistration.cs

--- AssesmentUC.Infrastructure/InfrastructureRegistration.cs
+++ AssesmentUC.Infrastructure/InfrastructureRegistration.cs
@@ -1,5 +1,7 @@
-﻿using AssesmentUC.Infrastructure.Repository.Impl;
+﻿using AssesmentUC.Infrastructure.Data;
+using AssesmentUC.Infrastructure.Repository.Impl;
 using AssesmentUC.Infrastructure.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,9 +9,23 @@ namespace AssesmentUC.Infrastructure
 {
     public static class InfrastructureRegistration
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
+            // Configurar SQL Server DbContext
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("BDPRACTICAS"),
+                    sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
+            // Configurar Oracle DbContext
+            services.AddDbContext<BannerDbContext>(options =>
+                options.UseOracle(
+                    configuration.GetConnectionString("BANNER"),
+                    oracleOptions => oracleOptions.UseOracleSQLCompatibility("11")));
+
+            // Registrar repositorios
             services.AddScoped<IEncuestaRepository, EncuestaRepository>();
             services.AddScoped<IRespuestaRepository, RespuestaRepository>();
             services.AddScoped<IReporteRepository, ReporteRepository>();
